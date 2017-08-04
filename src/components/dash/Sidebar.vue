e9bcb60eac121fc8a0e78f9b9e4491c63cb3d1c4
Kevin Liu <kevin07liu@gmail.com>
2017-08-04 11:33:05 +0800

    feat: Add fontawsome and implement the side-bar css

--- src/components/dash/Sidebar.vue
+++ src/components/dash/Sidebar.vue
@@ -1,5 +1,33 @@
 <template>
-  <h1>Sidebar</h1>
+  <div id="sidebar" class="text-center">
+    <h4 class="text-center">Username</h4>
+    <hr>
+    <div class="row">
+      <div class="col-sm-6">
+        <a href="#" class="btn btn-block btn-default"><i class="fa fa-user"></i> Profile</a>
+      </div>
+      <div class="col-sm-6">
+        <a href="#" class="btn btn-block btn-default"><i class="fa fa-user"></i> Newsfeed</a>
+      </div>
+    </div>
+    <div class="beepNowWrap m-t-20 m-b-20">
+      <textarea rows="10" class="form-control" maxlength="320" placeholder="Start writing your beep here!"></textarea>
+      <p class="text-muted">320 characters remaining</p>
+      <p class="text-center no-margin"><button class="btn btn-primary">Beep Now!</button></p>
+    </div>
+    <div class="row">
+      <div class="col-sm-6">
+        <p class="text-center no-margin">
+          <a href="#"><i class="fa fa-cog"></i> Settings</a>
+        </p>
+      </div>
+      <div class="col-sm-6">
+        <p class="text-center no-margin">
+          <a href="#"><i class="fa fa-power-off"></i> Logout</a>
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +36,21 @@
   }
 </script>
 
-<style>
-
+<style scoped>
+  #sidebar {
+    background: #fff;
+    padding: 20px;
+    border-radius: 6px;
+  }
+  .beepNowWrap {
+    background: #f0f0f0;
+    margin: 20px -20px;
+    padding: 20px;
+  }
+  .beepNowWrap textarea {
+    background: transparent;
+    border: transparent;
+    box-shadow: none;
+    resize: vertical;
+  }
 </style>
