a51c29d3cdcccca0cd3ff636f023c67a1bc79584
Kevin Liu <kevin07liu@gmail.com>
2017-08-04 11:01:55 +0800

    feat: setup new routes and ready for implement for new components

---
+++ src/components/dash/Dash.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <sidebar></sidebar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import Sidebar from './Sidebar.vue';
+
+  export default {
+    name: 'dashboard',
+    components: {
+      Sidebar
+    }
+  }
+</script>
+
+<style>
+
+</style>
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,21 @@
 import Auth from './components/auth/Auth.vue';
 import Login from './components/auth/Login.vue';
 import Register from './components/auth/Register.vue';
+import Dash from './components/dash/Dash.vue';
+import Newsfeed from './components/dash/Newsfeed.vue';
 
 export const routes = [
+  {
+    path: '/',
+    component: Dash,
+    redirect: 'newsfeed',
+    children: [
+      {
+        path: '/newsfeed',
+        component: Newsfeed
+      }
+    ]
+  },
   {
     path: '/auth',
     component: Auth,
