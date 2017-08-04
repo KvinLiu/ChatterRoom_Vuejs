910e3e75b2f0e56f25f11cee93cc421801c9a823
Kevin Liu <kevin07liu@gmail.com>
2017-08-04 14:26:50 +0800

    feat: BeepList pagination

--- src/components/dash/BeepList.vue
+++ src/components/dash/BeepList.vue
@@ -2,34 +2,55 @@
   <div>
     <div id="beepsWraper">
       <beep v-for="beep in beeps" :beep="beep"></beep>
+      <div v-if="beepsLoading" id="beepsLoading" class="text-center">
+        <i class="fa fa-spin fa-spinner"></i>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
   import Beep from './Beep.vue';
-    export default {
+  export default {
     name: 'BeepList',
     components: {
       Beep
     },
-
     data () {
       return {
-        beeps: []
+        beeps: [],
+        page: {},
+        beepsLoading: false
       }
     },
     methods: {
-      getBeeps () {
-        this.$http.get('/beeps')
+      getBeeps(page) {
+        this.beepsLoading = true;
+        this.$http.get(`/beeps?page=${page}`)
           .then((res) => {
-            this.beeps = res.body.data;
+            this.beeps = this.beeps.concat(res.body.data);
+            this.page = {current: res.body.current_page, last: res.body.last_page};
+            this.beepsLoading = false;
           })
+          .catch(() => {
+            this.beepsLoading = false;
+          });
+
+      },
+      handleScroll() {
+        if (document.body.scrollHeight - window.innerHeight - document.body.scrollTop === 0) {
+          if (this.page.current < this.page.last)
+            this.getBeeps(this.page.current + 1);
+        }
       }
     },
     created () {
       this.beeps = [];
-      this.getBeeps();
+      this.getBeeps(1);
+      window.addEventListener('scroll', this.handleScroll)
+    },
+    destroyed (){
+      window.removeEventListener('scroll', this.handleScroll)
     }
   }
 </script>
